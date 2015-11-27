acb3a35279c953a35c8e4d7c09bccc909d4bd80e
Alan Hansford <awhansford@gmail.com>
2015-11-26 19:28:59 -0600

    added test to confirm write operation

--- Scheduler5/src/sensors/sensors.c
+++ Scheduler5/src/sensors/sensors.c
@@ -1592,6 +1592,7 @@ sensor_cb_fnct Sensor_armDelayedCallback(void *         _self,
 
 static void * implement_Sensor_default_writeDataToSensor(struct Sensor * _self)
 {
+	printf("implement_Sensor_default_writeDataToSensor\n");
 	// get struct IO pointer in the correct IO type ... SENSOR_xxxx_IO_TYPE
 	struct SENSOR_DEFAULT_IO_TYPE * localIoStructPtr =
 	                                    Sensor_getIoStructPointer(_self);
@@ -1601,8 +1602,8 @@ static void * implement_Sensor_default_writeDataToSensor(struct Sensor * _self)
 	command_t * commandBufferPTR = Sensor_getIoCommandBufPointer(_self);
 	if ( commandBufferPTR == NULL ) { return NULL; }  // fail no cmd buffer
 
-	// comm address is usually set externally once just after new(Sensor)
 	// do not allow default sensor to attempt reads/writes to a NULL address
+	// comm address is usually set externally once just after new(Sensor)
 	void * address = IO_getAddress(localIoStructPtr);
 	if ( address == NULL ) { return NULL; }  // fail
 
